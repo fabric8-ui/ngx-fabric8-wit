d1172dd828361f873e2a30b7e9d6e2b90c22e580
Pete Muir <pmuir@bleepbleep.org.uk>
2017-02-25 05:37:43 -0500

    docs: Add jsdoc for WIT_API_URL

--- src/app/api/wit-api.ts
+++ src/app/api/wit-api.ts
@@ -1,3 +1,11 @@
 import { OpaqueToken } from '@angular/core';
 
+/**
+ * An OpaqueToken which is used to inject the WIT_API_URL in to this or any other module.
+ *
+ * For example, in your injectable constructor:
+ *
+ *   Inject(WIT_API_URL) apiUrl: string
+ *
+ */
 export let WIT_API_URL = new OpaqueToken('fabric8.wit.api.url');
