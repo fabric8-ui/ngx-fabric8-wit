5405b0d314bb90d3ab2d73567c85c45563294012
Pete Muir <pmuir@bleepbleep.org.uk>
2017-02-24 21:59:46 -0500

    chore(build): create fix-git-repo.sh

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -28,7 +28,7 @@ mkdir -p dist && docker run --detach=true --name=ngx-fabric8-wit-builder -e "FAB
 # In order to run semantic-release we need a non detached HEAD, see https://github.com/semantic-release/semantic-release/issues/329
 docker exec ngx-fabric8-wit-builder git checkout master
 # Try to fix up the git repo so that npm publish can build the gitHead ref in to package.json
-docker exec ngx-fabric8-wit-builder sh -c 'git show-ref --head --heads | while IFS=" " read -r hash name; do test ! -e "${GIT_DIR:-.git}/$name" && echo $hash > "${GIT_DIR:-.git}/$name"; done'
+docker exec ngx-fabric8-wit-builder sh -c ./fix-git-repo.sh
 
 # Build almigty-ui
 docker exec ngx-fabric8-wit-builder npm install
---
+++ fix-git-repo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git show-ref --head --heads | while IFS=' ' read -r hash name; do test ! -e "${GIT_DIR:-.git}/$name" && echo $hash > "${GIT_DIR:-.git}/$name"; done
