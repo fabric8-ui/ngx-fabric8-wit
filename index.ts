0ad6d1d280f7deda64e54b3de3590e0172b88d2f
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-14 16:41:21 -0400

    fix(spaces): export all of space model

--- index.ts
+++ index.ts
@@ -5,7 +5,14 @@ export { Contexts } from './src/app/contexts/contexts';
 export { ContextTypes } from './src/app/contexts/context-types';
 export { WIT_API_URL } from './src/app/api/wit-api';
 export { ProcessTemplate } from './src/app/models/process-template';
-export { Space, SpaceAttributes } from './src/app/models/space';
+export {
+  Space,
+  SpaceAttributes,
+  SpaceLink,
+  SpaceRelationships,
+  SpaceRelatedLink,
+  RelationalData
+} from './src/app/models/space';
 export { Team } from './src/app/models/team';
 export { SpaceService } from './src/app/spaces/space.service';
 export { Spaces } from './src/app/spaces/spaces';
