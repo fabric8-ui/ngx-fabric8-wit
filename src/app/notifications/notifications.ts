5714c734db58c89d4c31eadec6ee2c9287a11d8e
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-15 16:29:11 -0400

    fix(notifications): recent is a property

--- src/app/notifications/notifications.ts
+++ src/app/notifications/notifications.ts
@@ -3,12 +3,11 @@ import { Notification } from './notification';
 import { Observable } from 'rxjs';
 export class Notifications {
 
+  recent: Observable<Notification[]>;
+
   message(notification: Notification): Observable<NotificationAction> {
     throw new Error('Not yet implemented');
   }
 
-  recent(): Observable<Notification[]> {
-    throw new Error('Not yet implemented');
-  }
 
 }
