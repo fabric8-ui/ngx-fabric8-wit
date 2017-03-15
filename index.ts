da6479717a64375e190a6e9c6851781640f17851
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-15 19:40:47 -0400

    feat(spaces): add space name validators

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
-import { Notification } from './src/app/notifications/notification';
+// Contexts
 export { ContextType } from './src/app/contexts/context-type';
 export { Context } from './src/app/contexts/context';
 export { Contexts } from './src/app/contexts/contexts';
 export { ContextTypes } from './src/app/contexts/context-types';
+
+// API
 export { WIT_API_URL } from './src/app/api/wit-api';
+
+// Spaces
 export { ProcessTemplate } from './src/app/models/process-template';
 export {
   Space,
@@ -16,6 +20,10 @@ export {
 export { Team } from './src/app/models/team';
 export { SpaceService } from './src/app/spaces/space.service';
 export { Spaces } from './src/app/spaces/spaces';
+export { UniqueSpaceNameValidatorDirective} from './src/app/spaces/unique-space-name.directive';
+export { ValidSpaceNameValidatorDirective } from './src/app/spaces/valid-space-name.directive';
+
+// Notifications
 export { Notification } from './src/app/notifications/notification';
 export { NotificationAction } from './src/app/notifications/notification-action';
 export { Notifications } from './src/app/notifications/notifications';
---
+++ src/app/spaces/valid-space-name.directive.ts
@@ -0,0 +1,72 @@
+import { Observable, Subject } from 'rxjs';
+import { SimpleChanges, OnChanges, Directive, Input, forwardRef } from '@angular/core';
+import {
+  AbstractControl,
+  Validators,
+  Validator,
+  NG_ASYNC_VALIDATORS,
+  AsyncValidatorFn
+} from '@angular/forms';
+
+@Directive({
+  selector: '[validSpaceName][ngModel]',
+  providers: [{
+    provide: NG_ASYNC_VALIDATORS,
+    useExisting: forwardRef(() => ValidSpaceNameValidatorDirective), multi: true
+  }]
+})
+export class ValidSpaceNameValidatorDirective implements Validator, OnChanges {
+
+  static readonly ALLOWED_SPACE_NAMES = /^[a-zA-Z0-9_-]*$/;
+  static readonly MIN_SPACE_NAME_LENGTH = 4;
+  static readonly MAX_SPACE_NAME_LENGTH = 16;
+
+
+  @Input() validSpaceName: boolean;
+
+  private valFn: any;
+
+  constructor() {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    let change = changes['validSpaceName'];
+    if (change) {
+      this.valFn = validSpaceNameValidator();
+    } else {
+      this.valFn = Validators.nullValidator;
+    }
+  }
+
+  validate(control: AbstractControl): Observable<{ [key: string]: any }> {
+    return this.valFn(control);
+  }
+
+}
+
+export function validSpaceNameValidator(): AsyncValidatorFn {
+
+  let changed$ = new Subject<any>();
+
+  return (control: AbstractControl): Observable<{ [key: string]: any }> => {
+    changed$.next();
+    return control.valueChanges
+      .debounceTime(50)
+      .distinctUntilChanged()
+      .takeUntil(changed$)
+      .map(value => {
+        if (!control.value || control.value.toString().length > ValidSpaceNameValidatorDirective.MAX_SPACE_NAME_LENGTH) {
+          return { maxLength: { valid: false, requestedName: control.value } };
+        }
+        let strVal: string = control.value.toString();
+        if (strVal.length < ValidSpaceNameValidatorDirective.MIN_SPACE_NAME_LENGTH) {
+          return { minLength: { valid: false, requestedName: control.value } };
+        } else if (!strVal.match(ValidSpaceNameValidatorDirective.ALLOWED_SPACE_NAMES)) {
+          return { invalid: { valid: false, requestedName: control.value } };
+        }
+        return null;
+      })
+      .first();
+  };
+}
