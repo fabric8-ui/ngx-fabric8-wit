a14f9a22e6e184b6aa4166e31e27d8aff2230ab8
Pete Muir <pmuir@bleepbleep.org.uk>
2017-04-25 13:03:05 -0400

    fix: need a global replace

--- src/app/spaces/space-name.pipe.ts
+++ src/app/spaces/space-name.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'spaceName', pure: true })
 export class SpaceNamePipe implements PipeTransform {
   transform(name: string): any {
-    return name ? name.replace('_', ' ') : name;
+    return name ? name.replace(/ /g, ' ') : name;
   }
 }
