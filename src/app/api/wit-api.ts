5831c3b0d2687105ce3627c6ef6f218133952ee0
Dana Gutride <dgutride@gmail.com>
2018-07-18 17:19:25 -0400

    fix(token): move to injection token (#176)

--- src/app/api/wit-api.ts
+++ src/app/api/wit-api.ts
@@ -1,11 +1,11 @@
-import { OpaqueToken } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 
 /**
- * An OpaqueToken which is used to inject the WIT_API_URL in to this or any other module.
+ * An InjectionToken which is used to inject the WIT_API_URL in to this or any other module.
  *
  * For example, in your injectable constructor:
  *
  *   Inject(WIT_API_URL) apiUrl: string
  *
  */
-export let WIT_API_URL = new OpaqueToken('fabric8.wit.api.url');
+export let WIT_API_URL = new InjectionToken<string>('fabric8.wit.api.url');
